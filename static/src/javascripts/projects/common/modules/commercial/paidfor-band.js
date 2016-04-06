124a6f7700e2a2781e120c5fafad474aed2fa2b1
Zofia <zofia.korcz@guardian.co.uk>
2016-04-06 12:56:27 +0100

    band disabled on interactive pages

--- static/src/javascripts/projects/common/modules/commercial/paidfor-band.js
+++ static/src/javascripts/projects/common/modules/commercial/paidfor-band.js
@@ -1,12 +1,18 @@
 define([
     'fastdom',
     'Promise',
+    'common/utils/config',
     'common/modules/ui/sticky'
-], function (fastdom, Promise, Sticky) {
+], function (
+    fastdom,
+    Promise,
+    config,
+    Sticky
+) {
     function init() {
         return new Promise(function (resolve) {
             var elem = document.querySelector('.facia-page > .paidfor-band, #article > .paidfor-band');
-            if (elem) {
+            if (elem && config.page.contentType !=='Interactive') {
                 new Sticky(elem).init();
             }
             resolve();
