9af4080d6f734838564343a54f0afb1bd8534239
Francis Carr <francis.carr@guardian.co.uk>
2016-05-24 15:03:42 +0100

    Remove FormstackHttpResponse

--- identity/app/formstack/FormstackHttp.scala
+++ identity/app/formstack/FormstackHttp.scala
@@ -3,19 +3,15 @@ package formstack
 import com.google.inject.Singleton
 import common.ExecutionContexts
 import play.api.Play.current
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WSResponse, WS}
 
 import scala.concurrent.Future
 
-case class FormstackHttpResponse(body: String, status: Int, statusText: String)
-
 @Singleton
 class WsFormstackHttp extends ExecutionContexts {
-  def get(url: String, parameters: Seq[(String, String)] = Nil): Future[FormstackHttpResponse] = {
+  def get(url: String, parameters: Seq[(String, String)] = Nil): Future[WSResponse] = {
     WS.url(url)
       .withRequestTimeout(2000)
       .withQueryString(parameters:_*)
-      .get()
-      .map(response => FormstackHttpResponse(response.body, response.status, response.statusText))
-  }
+      .get()}
 }
