0986b4a1041363b6e1c6971a6265bc689e7bdfea
Francis Carr <francis.carr@guardian.co.uk>
2016-05-24 14:34:28 +0100

    Remove unnecessary trait FormstackHttp

--- identity/app/formstack/FormstackHttp.scala
+++ identity/app/formstack/FormstackHttp.scala
@@ -7,15 +7,11 @@ import play.api.libs.ws.WS
 
 import scala.concurrent.Future
 
-trait FormstackHttp {
-  def GET(url: String, headers: Seq[(String, String)] = Nil): Future[FormstackHttpResponse]
-}
-
 case class FormstackHttpResponse(body: String, status: Int, statusText: String)
 
 @Singleton
-class WsFormstackHttp extends FormstackHttp with ExecutionContexts {
-  override def GET(url: String, parameters: Seq[(String, String)] = Nil): Future[FormstackHttpResponse] = {
+class WsFormstackHttp extends ExecutionContexts {
+  def GET(url: String, parameters: Seq[(String, String)] = Nil): Future[FormstackHttpResponse] = {
     WS.url(url)
       .withRequestTimeout(2000)
       .withQueryString(parameters:_*)
