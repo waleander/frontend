70d4e3a909208780f46aa4826fd2982ff3f09d07
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-07-26 09:15:22 +0100

    Revert "add video caption a/b test"

--- static/src/javascripts/projects/common/modules/experiments/tests/video-caption.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/video-caption.js
@@ -1,46 +1,36 @@
-define([
-    'common/utils/config',
-    'qwery',
-    'bonzo',
-    'common/utils/$'
-], function (
-    config,
+define(['qwery',
+        'common/utils/config'
+], function(
     qwery,
-    bonzo,
-    $
-    ){
-    return function()
-    {
+    config
+
+) {
+    return function() {
         this.id = 'VideoCaption';
-        this.start = '2016-07-22';
+        this.start = '2016-07-18';
         this.expiry = '2016-07-25';
         this.author = 'Gideon Goldberg';
-        this.description = 'Increase the prominence of the video caption on in-article videos.';
-        this.audience = 0.06;
-        this.audienceOffset = 0.1;
-        this.successMeasure = 'Video starts.';
-        this.audienceCriteria = 'Users viewing an article with a video embedded.';
+        this.showForSensitive = true;
+        this.description = 'Test if increasing the prominence of the video caption on in-article videos leads to more plays.';
+        this.audience = 1;
+        this.audienceOffset = 0;
+        this.successMeasure = 'Video starts';
+        this.audienceCriteria = '';
         this.dataLinkNames = '';
-        this.idealOutcome = 'Video starts are increased';
+        this.idealOutcome = '';
         this.canRun = function() {
             return config.page.contentType === 'Article' &&  qwery('[data-component="main video"]').length > 0;
         };
 
         this.variants = [
             {
-                id: 'control',
+                id: 'baseline1',
                 test: function () {
                 }
             },
             {
-                id: 'caption-overlay',
+                id: 'baseline2',
                 test: function () {
-                }
-            },
-            {
-                id: 'caption-larger-text',
-                test: function () {
-                    bonzo($('.caption--main a')).addClass('caption--large');
                 }
             }
         ];
